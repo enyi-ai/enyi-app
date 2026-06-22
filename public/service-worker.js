b2092850bc97963778ea76ede00e8790c21ad2d4
Lucky Aziken <luckyaziken@Mac.home>
2026-06-22 08:15:42 +0100

    adding service worker

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'enyi-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon-96x96.png',
+  '/apple-touch-icon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
